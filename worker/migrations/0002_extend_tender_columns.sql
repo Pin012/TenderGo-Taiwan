88b0c8ec2d4b2b519b6f1b1aab62dc439fe6782a
Pin012 <husk9612@gmail.com>
2026-05-13 16:16:46 +0800

    fix(worker): preserve tender id when decoding pageCode2Img cells

---
+++ worker/migrations/0002_extend_tender_columns.sql
@@ -0,0 +1,6 @@
+ALTER TABLE tenders ADD COLUMN serial_no TEXT;
+ALTER TABLE tenders ADD COLUMN bidding_method TEXT;
+ALTER TABLE tenders ADD COLUMN award_status TEXT;
+ALTER TABLE tenders ADD COLUMN award_amount_text TEXT;
+ALTER TABLE tenders ADD COLUMN award_amount_value INTEGER;
+ALTER TABLE tenders ADD COLUMN winning_vendor TEXT;
